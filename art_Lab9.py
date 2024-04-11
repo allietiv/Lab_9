87157af3bd87ab7584eecd7bfe82519c8d61a88f
gabri <gabrielahouser1@gmail.com>
2024-04-11 11:20:47 -0400

    added stuff

--- art_Lab9.py
+++ art_Lab9.py
@@ -7,6 +7,13 @@ def encode(password):
         encoded_password += encoded_digit
     return encoded_password
 
+def decode(encoded_password):
+    decoded_password = ""
+    for digit in encoded_password:
+        encoded_digit = str(int(digit) - 3)
+        decoded_password += encoded_digit
+    return decoded_password
+
 def main():
     while True:
         print("Menu")
@@ -21,8 +28,10 @@ def main():
             password = input("Please enter your password: ")
             encoded_password = encode(password)
             print("Your password has been encoded and stored!")
+            print()
         elif option == 2:
-            return
+            print(f"The encoded password is {encoded_password}, and the original password is {decode(encoded_password)}.")
+            print()
         elif option == 3:
             break
 
